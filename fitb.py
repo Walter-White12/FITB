0c4973f08e2770d8d83f7e5c9d9038be137bfc3b
SujalBajra <sujalbajracharya1@gmail.com>
2023-01-04 16:57:36 +0545

    readme almost finished

---
+++ fitb.py
@@ -0,0 +1,19 @@
+"""
+Problem:
+    __ __
+  *    __
+    -----
+    __ __
+  + __ __
+    -----
+    __ __
+"""
+
+solution = False
+count = 6
+
+blanks = ['', '', '', '', '', '', '', '', '']
+values = [1, 2, 3, 4, 5, 6 ,7, 8, 9]
+
+while not solution:
+   pass
